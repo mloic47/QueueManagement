f592d5984ddb036c4f1252c13c8aeef32c8980d4
mloic47 <mloic47@gmail.com>
2023-05-23 19:30:25 +0100

    simple gui completed

---
+++ src/main/java/com/mycompany/queuemanagement/QueueManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.queuemanagement;
+
+/**
+ *
+ * @author mloic47
+ */
+public class QueueManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
